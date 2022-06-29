7009856adccbc9353cbdf4748eb2bdc84cfd6205
renestimu <byumitsiz@gmail.com>
2022-06-29 21:35:37 +0300

    Update

---
+++ Core/StudyCase.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,21 @@
+﻿using StudyCase.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyCase.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        Task<bool> AddAsync(T model);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Update(T model);
+        bool Remove(T model);
+        bool RemoveRange(List<T> datas);
+        Task<bool> Remove(string id);
+        Task<int> SaveAsync();
+
+    }
+}
